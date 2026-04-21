5fde9caf97a635a5ea1d0a0f1eadf476f08dd3a6
Airon Herman <aironherman9@gmail.com>
2026-04-21 13:12:52 -0400

    Add project files.

---
+++ Model/Schedule.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace trainStation.Model
+{
+    public class Schedule
+    {
+        //Primary Key
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime Arrival { get; set; } = DateTime.Now;
+
+        public DateTime Departure { get; set; } = DateTime.Now;
+
+
+        [EnumDataType(typeof(Type))]
+        public Type? Platform { get; set; }
+
+        //Foreign Key
+        [Display(Name = "Train")]
+        public int TrainID { get; set; }
+        public Train? Train { get; set; }
+    }
+}
---
+++ Model/Train.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace trainStation.Model
+{
+    public class Train
+    {
+        //Primary Key
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public string Company { get; set; } = string.Empty;
+
+        [Required]
+        [EnumDataType(typeof(Type))]
+        public Type? Service { get; set; }
+
+        [EnumDataType(typeof(Type))]
+        public Type? Cargo { get; set; }
+
+        [Required]
+        public double Length { get; set; } //In Meters
+        [Required]
+        public double Mass { get; set; } //In Kilograms
+
+        public bool specialHandling { get; set; }
+
+        //Navigation Property
+        public ICollection<Schedule> Schedule { get; set; } = new List<Schedule>();
+    }
+}
